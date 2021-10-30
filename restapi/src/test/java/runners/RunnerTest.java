3ab80a34ac5cfeb77df0eeb6f3d877c2ea6bcc6e
Sukanya Ravinder <sukanya.ravinder@gmail.com>
2021-10-30 09:43:47 -0500

    recent run

--- restapi/src/test/java/runners/RunnerTest.java
+++ restapi/src/test/java/runners/RunnerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/functionalTests",
@@ -15,6 +16,6 @@ import io.cucumber.junit.CucumberOptions;
         		        "json:target/cucumber/report.json","rerun:target/failed_scenarios.txt" },
         monochrome = true
 )
-public class TestRunner {
+public class RunnerTest {
 
 }
