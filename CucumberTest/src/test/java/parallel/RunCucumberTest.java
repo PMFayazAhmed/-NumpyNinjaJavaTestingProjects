52d53326ad7ef21da86b446c05865cb426895d3d
Sukanya Ravinder <sukanya.ravinder@gmail.com>
2021-10-30 14:36:19 -0500

    first commmit

---
+++ CucumberTest/src/test/java/parallel/RunCucumberTest.java
@@ -0,0 +1,19 @@
+package parallel;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/parallel",
+        glue = {"StepDefinition"},
+        plugin = { "pretty", "html:target/cucumber/report.html",
+        		        "json:target/cucumber/report.json","rerun:target/failed_scenarios.txt" },
+        monochrome = true
+)
+public class RunCucumberTest {
+
+}
